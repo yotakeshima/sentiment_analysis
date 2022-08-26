703c3acc6b7835dfeb6f6df73faf3d98a216109e
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-08-26 01:54:44 -0400

    finally split data

--- src.py
+++ src.py
@@ -1,3 +1,8 @@
+import re
 import numpy as np
 
-print("hello")
+f = open("trainhw1new.txt","r")
+reviews = f.readlines()[1:10]
+for line in reviews:
+    text = re.split(r"\t\w", line)
+    print(text[0])
