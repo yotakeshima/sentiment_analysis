f517f9f1ce0128dccc92937a166bd5f6d97ffee5
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-08-27 02:17:48 -0400

    finally got it

--- src.py
+++ src.py
@@ -1,12 +1,14 @@
 import re
 import numpy as np
+import pandas as pd
 
 f = open("trainhw1new.txt","r")
 reviews = f.readlines()[1:10]
 for line in reviews:
-    text = re.split(r"\t\w", line)
-    text = text[0]
-    text = text.replace(" ", "")
-    text = text.replace(".", "")
-    textToInt = int(text)
-    print(textToInt + 1)
+    text = re.split(r"\t", line)
+    print(text[1] + '\n')
+    ratings = text[0]
+    comments = text[1]
+    ratings = ratings.replace(" ", "")
+    ratings = ratings.replace(".", "")
+print(reviews)
