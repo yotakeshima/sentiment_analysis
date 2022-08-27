7ddccc51cf02c280c8f0e573c57869bd3e38e3f4
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-08-27 02:00:03 -0400

    text to int

--- src.py
+++ src.py
@@ -5,4 +5,8 @@ f = open("trainhw1new.txt","r")
 reviews = f.readlines()[1:10]
 for line in reviews:
     text = re.split(r"\t\w", line)
-    print(text[0])
+    text = text[0]
+    text = text.replace(" ", "")
+    text = text.replace(".", "")
+    textToInt = int(text)
+    print(textToInt + 1)
