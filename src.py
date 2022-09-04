9db498615d67cb6784e7e8ae365a7b247b715cfa
Yoshi <yotakeshima@gmail.com>
2022-09-04 13:19:48 -0400

    split by dataframes

--- src.py
+++ src.py
@@ -3,4 +3,5 @@ import numpy as np
 import pandas as pd
 import string
 
-df = pd.read_csv('trainhw1new.txt', names=['Labels', 'Reviews'], sep='(?<=\d)\t|(?<=.)\t') 
+df = pd.read_csv('trainhw1new.txt', names=['Labels', 'Reviews'], sep='(?<=\d)\t|(?<=.)\t', usecols=range(2)) 
+df.head()
