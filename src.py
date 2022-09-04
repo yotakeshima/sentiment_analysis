2339ac28228efb57a2729284fcfb775bc3475fc0
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-09-04 19:48:38 -0400

    reviews iteration through kfolds

--- src.py
+++ src.py
@@ -5,14 +5,14 @@ import pandas as pd
 from sklearn.model_selection import KFold
 import string
 
-df = pd.read_csv('trainhw1new.txt', names=['Labels', 'Reviews'], sep='(?<=\d)\t|(?<=.)\t', usecols=range(2)) 
-kf = KFold(n_splits=10)
-result = next(kf.split(df), None)
-train = df.iloc[result[0]]
+df = pd.read_csv('trainhw1new.txt', names=['Labels', 'Reviews'], sep='(?<=\d)\t|(?<=.)\t', usecols=range(2)) # Create a dataframe from trainhw1new.txt file. Then split the data into 2 columns by 'Labels' | 'Reviews' 
+kf = KFold(n_splits=10)                     # initialize kf variable, splits into k=10 folds
+result = next(kf.split(df), None)           # result variable takes just one of the Kfold splits.
+train = df.iloc[result[0]]                  
 test = df.iloc[result[1]]
 
-#print(test)
-#print(train)
+# print(test)
+# print(train)
 
 for train_idx, test_idx in kf.split(df):
-    print(df.iloc[train_idx])
+    print(df.iloc[test_idx]['Reviews'])
