04bb6b01709ed7f5001b97d939e8bada321cb0a8
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-09-04 19:40:02 -0400

    figured out kfold

--- src.py
+++ src.py
@@ -1,7 +1,18 @@
 import re
+from tkinter import N
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import KFold
 import string
 
 df = pd.read_csv('trainhw1new.txt', names=['Labels', 'Reviews'], sep='(?<=\d)\t|(?<=.)\t', usecols=range(2)) 
-df.head()
+kf = KFold(n_splits=10)
+result = next(kf.split(df), None)
+train = df.iloc[result[0]]
+test = df.iloc[result[1]]
+
+#print(test)
+#print(train)
+
+for train_idx, test_idx in kf.split(df):
+    print(df.iloc[train_idx])
