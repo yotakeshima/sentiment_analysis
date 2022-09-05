52b4d6992db4eea64db89b10ca732842c8f30767
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-09-05 16:11:43 -0400

    vectorize in progress, finish adding cosine similarity column in dataframe

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import KFold
@@ -14,11 +12,22 @@ import scipy.sparse
 def preprocess(text):
     text.loc[:,"Reviews"] = text.Reviews.apply(lambda x : str.lower(x))
     text.loc[:,"Reviews"] = text.Reviews.apply(lambda x : " ".join(re.findall('[\w]+',x)))
+    text.loc[:,"Labels"] = text.Labels.apply(lambda x : "".join(re.findall('[\d]|\-',x)))
     return text
 def testPrep(text):
     text.iloc[1] = text.iloc[1].lower()
     text.iloc[1] = text.iloc[1].translate(str.maketrans('','', string.punctuation))
     return text
+
+def vectorize(data):
+    idf = TfidfVectorizer()
+    idf_matrix = idf.fit_transform(data.Reviews)
+    cv = CountVectorizer(stop_words='english')
+    cv_matrix = cv.fit_transform(data.Reviews)
+    vectors = scipy.sparse.hstack([cv_matrix, idf_matrix])
+    vectors = vectors.toarray()
+    data.insert(2,"Vectors", vectors, True)
+
 
 df = pd.read_csv('trainhw1new.txt', names=['Labels', 'Reviews'], sep='(?<=\d)\t|(?<=.)\t', usecols=range(2)) # Create a dataframe from trainhw1new.txt file. Then split the data into 2 columns by 'Labels' | 'Reviews' 
 test = df.iloc[0]
@@ -26,27 +35,29 @@ train = df.iloc[1:10]
 
 train = preprocess(train)
 test = testPrep(test)
+new_train = train[['Labels','Reviews']].copy()
 
-similar = []
+
+print(train)
+print(new_train)
 
 
 
 # train.loc[:,"Reviews"] = train.Reviews.apply(lambda x : str.lower(x))
 # train.loc[:,"Reviews"] = train.Reviews.apply(lambda x : " ".join(re.findall('[\w]+',x)))
 
-# test = "I love being pummeled by ugly people"
-# train = "I love being killed by ugly people"
-
 
 # idf = TfidfVectorizer()
-# idf_matrix = idf.fit_transform(matrix)
+# idf_matrix = idf.fit_transform([new_train.iloc[0]['Reviews'],new_train.iloc[1]['Reviews']])
+
 
 # cv = CountVectorizer(stop_words='english')
-# cv_matrix = cv.fit_transform(matrix)
+# cv_matrix = cv.fit_transform([new_train.iloc[0]['Reviews'],new_train.iloc[1]['Reviews']])
 
 
 # X = scipy.sparse.hstack([cv_matrix, idf_matrix])
 # X = X.toarray()
+# print(X)
 
 # result = spatial.distance.cosine(X[0], X[1])
 # print(result)
